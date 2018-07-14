eebfff3f5e8a8bfb2db8cb7898b13b8f3bf1b437
Llevella <irinavkhomutova@gmail.com>
2018-07-14 22:23:30 +0400

    вытаскивает друзей и выводит их

--- ex3-3/ex3_3.py
+++ ex3-3/ex3_3.py
@@ -1,1 +1,26 @@
+import requests
+import urllib
 
+APP_ID = '6633040'
+AUTH_SERVER = 'https://api.vk.com/authorize'
+TOKEN = '04cdbc464ff1a0e9fc8b02684f7585927e4c6553d9aeb55293303707c97e49d2f25c620f810a3a0de0de5'
+auth_data = {
+    'client_id': APP_ID,
+    'display': 'popup',
+    'redirect_uri': 'https://oauth.vk.com/blank.html',
+    'scope': 'friends',
+    'response_type': 'token',
+    'v': '5.80'
+    }
+
+FRIENDS_SERVER = 'https://api.vk.com/method/friends.get'
+friend_get_params = {
+    'order': 'name',
+    'fields': 'city',
+    'ACCESS_TOKEN': TOKEN,
+    'v': '5.80'
+    }
+
+requests_for_friends = requests.get(FRIENDS_SERVER, params=friend_get_params)
+#dict_friends_from_request = json.loads(requests_for_friends.text)
+print(requests_for_friends.text)
